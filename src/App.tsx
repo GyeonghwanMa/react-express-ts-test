f39abdf8b32444ba7ed241632adc9a10f07499a8
kyunghwanma <makh1213@naver.com>
2021-06-04 23:15:01 +0900

    1. ReducerSample 구현하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Greetings from './Greetings';
 import Counter from './Counter';
 import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 function App() {
   // const onClick = (name: string) => {
@@ -11,9 +12,9 @@ function App() {
     console.log(form);
   }
   // return <Greetings name="Hello" onClick={onClick} />;
-  return <Counter></Counter>
+  // return <Counter></Counter>
   // return <MyForm onSubmit={onSubmit}></MyForm>
-  
+  return <ReducerSample></ReducerSample>
 };
 
 export default App;
